ddf0ae81ba3e6bc3efa5e31b84a8edf147e7a08d
ABauschat <abthebest13@googlemail.com>
2024-11-17 21:14:17 +0100

    17.11.24 21:14

--- src/HandleEvents.cpp
+++ src/HandleEvents.cpp
@@ -29,9 +29,6 @@ namespace NuggetsInc
 
     void HandleEvents::executeCommand(const char* command, const char* data)
     {
-        struct_message response;
-        strcpy(response.messageType, "response");
-
         if (strcmp(command, "MOVE_UP") == 0)
         {
 
@@ -46,36 +43,11 @@ namespace NuggetsInc
         }
         else if (strcmp(command, "MOVE_RIGHT") == 0)
         {
-            
+
         }
         else if (strcmp(command, "SELECT") == 0)
         {
 
         } 
-        else
-        {
-            strcpy(response.command, "UNKNOWN_COMMAND");
-        }
-
-        if (data != nullptr)
-        {
-            strncpy(response.data, data, sizeof(response.data) - 1);
-            response.data[sizeof(response.data) - 1] = '\0'; 
-        }
-        else
-        {
-            response.data[0] = '\0'; 
-        }
-
-        if (connectWithRemote)
-        {
-            connectWithRemote->sendData(reinterpret_cast<const uint8_t*>(&response), sizeof(response));
-            Serial.print("Sent response: ");
-            Serial.println(response.command);
-        }
-        else
-        {
-            Serial.println("Connector not set");
-        }
     }
 }
